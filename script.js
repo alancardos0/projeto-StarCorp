9a0075df96955255946a3cd52ca0e9e6652e8fcd
Cardoso <92821038+alancardos0@users.noreply.github.com>
2022-03-29 16:52:31 -0300

    projeto-Starcorp

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ import { cadastroDeEndereco } from "./main.js";
 const url = `https://www.selida.com.br/avaliacaotecnica/`;
 
 class Api {
-  static async getAdress() {
+  static getAdress() {
     const inserirCliente = axios
-      .get(url)
+      .get(`${url}api/Endereco`, {
+        Authorization: "AED57B57-F588-4AB7-AD02-DD99B49D44AF",
+      })
       .then((res) => {
-        console.log(res.data);
-        axios.put(`${url}endereco`, cadastroDeEndereco);
+        const data = res.data;
+        console.log(data);
       })
       .catch((error) => {
         console.error(error);
@@ -18,3 +20,25 @@ class Api {
 }
 
 Api.getAdress();
+
+/*
+function addNewUser() {
+  axios
+    .post(`${url}api/Endereco`, {
+      Authorization: "AED57B57-F588-4AB7-AD02-DD99B49D44AF",
+      logradouro: "Alan",
+      numero: "Matheus",
+      bairro: "cardoso",
+      cidade: "do",
+      uf: "Amaral",
+    })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+addNewUser();
+*/
